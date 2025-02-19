527ff9778f0aefced15c4c54e2909adef6f28b84
revenancio <sauldavalos23@gmail.com>
2025-02-19 13:39:08 -0500

    creacion de codigo para el modelo

--- Mvc/MODELO/Modelo.py
+++ Mvc/MODELO/Modelo.py
@@ -1,1 +1,37 @@
-print("hi") 
+class LolItem: 
+    def _init__(self, id, nombre, precio, tipo, penetacion=0, dano_magico=0, danocritico=0):
+        self.id = id
+        self.nombbre = nombre
+        self.precio = precio
+        self.tipo = tipo
+        self.penetacion = penetacion
+        self.dano_magico = dano_magico
+        self.danocritico = danocritico
+
+#simulacion de base de datos 
+items = [] #lista donde se almacenan los nuevos objetos creados
+
+def create_item(id, nombre, precio, tipo, penetacion=0, dano_magico=0, danocritico=0):
+    item = LolItem(id, nombre, precio, tipo, penetacion, dano_magico, danocritico)
+    items.append(item)
+    return item
+def read_item(id):
+    for item in items:
+        if item.id == id:
+            return item
+    return None
+
+def update_item(id, nombre, precio, tipo, penetacion, dano_magico, danocritico):
+    for item in items:
+        if item.id == id:
+            item.nombre = nombre
+            item.precio = precio
+            item.tipo = tipo
+            item.penetacion = penetacion
+            item.dano_magico = dano_magico
+            item.danocritico = danocritico
+            return item
+        
+def delete_item(nombre):
+    global items
+    item = [item for item in items if item.nombre != nombre]
--- README.md
+++ README.md
@@ -1,2 +1,36 @@
-# crud
-crud con pythnhola papu
+class LoLItem:
+    def __init__(self, id, nombre, precio, tipo, penetracion=0, dano_magico=0, dano_critico=0):
+        self.id = id
+        self.nombre = nombre
+        self.precio = precio
+        self.tipo = tipo
+        self.penetracion = penetracion
+        self.dano_magico = dano_magico
+        self.dano_critico = dano_critico
+
+# Simulación de base de datos
+items = []
+
+def create_item(id, nombre, precio, tipo, penetracion, dano_magico, dano_critico):
+    item = LoLItem(id, nombre, precio, tipo, penetracion, dano_magico, dano_critico)
+    items.append(item)
+    return item
+
+def read_items():
+    return items
+
+def update_item(id, nombre, precio, tipo, penetracion, dano_magico, dano_critico):
+    for item in items:
+        if item.id == id:
+            item.nombre = nombre
+            item.precio = precio
+            item.tipo = tipo
+            item.penetracion = penetracion
+            item.dano_magico = dano_magico
+            item.dano_critico = dano_critico
+            return item
+    return None
+
+def delete_item(id):
+    global items
+    items = [item for item in items if item.id != id]
